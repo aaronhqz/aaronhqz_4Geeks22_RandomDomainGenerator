be76c33c8d7c60167a2c049a12bd46bf16f3915a
Aaron Henriquez <60531374+aaronhqz@users.noreply.github.com>
2023-10-12 00:31:20 +0000

    Creation of html file and app.js with the required code for the domain names

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["Del", "Nuestro", "Tu"];
+  let adj = ["Oscuro", "Gran", "Gracioso", "Lindo"];
+  let noun = ["Hombre", "Mujer", "Pez", "Raton", "Gato", "Fama"];
+  let end = [".com", ".cr", ".ca", ".mx", ".net"];
+
+  let listOfDomains = [];
+
+  for (let pronouns of pronoun) {
+    for (let adjetives of adj) {
+      for (let nouns of noun) {
+        for (let ends of end) {
+          listOfDomains.push(pronouns + adjetives + nouns + ends);
+        }
+      }
+    }
+  }
+
+  let randomDomainName =
+    listOfDomains[Math.floor(Math.random() * listOfDomains.length - 1)];
+
+  document.getElementById("hola").innerHTML = randomDomainName;
+  document.getElementById("button").addEventListener("click", function() {
+    location.reload();
+    alert("Here is a better one: ");
+  });
 };
